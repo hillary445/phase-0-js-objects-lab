7780550ce6b4e21e6863a230c4a8061e04b0bbb2
hillary445 <hilaryprit02@gmail.com>
2025-11-23 13:07:59 +0300

    Created an event management system to log in attendees

--- index.js
+++ index.js
@@ -1,4 +1,44 @@
 //Write your code here
+let attendee = {
+  attendeeId : "T001",
+  name : "Alice Smith",
+  event : "JavaScript Conference",
+  ticketType : "VIP",
+  ticketPrice : 150.00
+}
+
+//Function to log attendee name
+function logAttendeeName(attendee){
+  console.log(attendee.name);
+}
+
+//Function to log ticket price
+function logTicketPrice(attendee){
+  console.log(attendee.ticketPrice);
+}
+
+//Function to update ticket type
+function updateTicketType(attendee){
+  attendee.ticketType = "General"
+}
+
+//Function to Update Ticket Price
+function updateTicketPrice(attendee){
+  attendee.ticketPrice = 200.00
+}
+
+//Function to remove the event proeperty
+function removeEventProperty(attendee){
+  delete attendee.event;
+}
+
+//Function to add a checked in property
+function addCheckedInProperty( attendee){
+  attendee.checkedIn = true;
+}
+
+
+
 
 
 
